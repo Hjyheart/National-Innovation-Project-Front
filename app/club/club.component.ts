19c60e5020ce3d68281c90d24a5e461ffb5d0cc8
洪嘉勇 <tjhjyisland@gmail.com>
2017-02-21 00:04:16 +0800

    init

---
+++ app/club/club.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-club',
+  templateUrl: './club.component.html',
+})
+export class ClubComponent {}
---
+++ app/home/home.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-home',
+  templateUrl: './home.component.html',
+})
+export class HomeComponent {}
